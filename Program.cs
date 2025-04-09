36b52121bf8d241f5c73d0660a9b3c829bb25203
Ali <n00414045@upn.pe>
2025-04-09 10:03:56 -0500

    primer commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("HOLA PENDEJO");
+            Console.ReadKey();
+        }
+    }
+}
